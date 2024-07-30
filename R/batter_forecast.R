5c473061c2f4f72a83ce97868bc758c3da336b38
michael hagen <168500692+itsmichaelhagen@users.noreply.github.com>
2024-07-29 22:15:25 -0500

    Add files via upload
    Added new forecasting methods. Available to forecast for AAA and AA players.

--- R/batter_forecast.R
+++ R/batter_forecast.R
@@ -5,7 +5,7 @@ library(forecast)
 library(ggplot2)
 
 # SIMPLE LINEAR REGRESSION MODEL ANALYSIS OF PAST WEEK GAMES - HITTER FORECAST
-## Still needs a lot of work
+## Available for MLB, AAA and AA players
 
 #Working Directory
 setwd("~/Documents/Data Analytics/Baseball-Analytics-with-R-Python-and-SQL")
@@ -23,39 +23,161 @@ getRoster <- function (t) {
   currentRoster <<- mlb_rosters(team_id = t, season = 2024, roster_type = 'active')
 }
 
-plot_forecast_10_game <- function() {
-  my_linear_model <- lm(hits~game, data = hitter_forecast_10_games)
+getGamesPlayedML <- function() {
+  gamesPlayedML_df <<- majorleague_pbp[(majorleague_pbp$matchup.batter.id %in% mlbid),]
+  gamesPlayedML <<- as.vector(gamesPlayedML_df$game_pk)
+  gamesPlayedML <<- unique(gamesPlayedML)
+}
+
+getGamesPlayedAAA <- function() {
+  gamesPlayedAAA_df <<- triplea_pbp[(triplea_pbp$matchup.batter.id %in% mlbid),]
+  gamesPlayedAAA <<- as.vector(gamesPlayedAAA_df$game_pk)
+  gamesPlayedAAA <<- unique(gamesPlayedAAA)
+}
+
+getGamesPlayedAA <- function() {
+  gamesPlayedAA_df <<- doublea_pbp[(doublea_pbp$matchup.batter.id %in% mlbid),]
+  gamesPlayedAA <<- as.vector(gamesPlayedAA_df$game_pk)
+  gamesPlayedAA <<- unique(gamesPlayedAA)
+}
+
+plot_forecast_avg <- function() {
+  my_linear_model <<- lm(bavg~game, data = hitter_forecast_avg)
   
-  my_forecast <- forecast(my_linear_model, nwd_10, lambda = 0, biasadj = FALSE, h = 10)
+  my_forecast <<- forecast(my_linear_model, nwd_avg, lambda = 0, biasadj = FALSE, h = 2)
   
-  plot(my_forecast, type = "l", main = games_10_title, xlab = "Hits", ylab = "Game")
+  x_avg <<- my_linear_model[["coefficients"]][["(Intercept)"]]
+  y_avg <<- my_linear_model[["coefficients"]][["game"]]
+  
+  ba_forecasted <<- data.frame(
+    series = c(1, 2, 3, 4, 5, 6),
+    bavg = c(sum(batter_stats$hits[16:20])/sum(batter_stats$at_bats[16:20]), sum(batter_stats$hits[11:15])/sum(batter_stats$at_bats[11:15]), sum(batter_stats$hits[6:10])/sum(batter_stats$at_bats[6:10]), sum(batter_stats$hits[1:5])/sum(batter_stats$at_bats[1:5]), (x_avg+y_avg*5), (x_avg+y_avg*6))
+  )
+  
+  plot(my_forecast, type = "l", main = games_10_title, xlab = "Series", ylab = "Batting Average")
+  
+}
+
+plot_forecast_next_game_hits <- function() {
+  my_linear_model <<- lm(hits~game, data = hitter_forecast_next_game)
+  
+  my_forecast <<- forecast(my_linear_model, nwd_ng, lambda = 0, biasadj = FALSE, h = 10)
+
+  x_avg <<- my_linear_model[["coefficients"]][["(Intercept)"]]
+  y_avg <<- my_linear_model[["coefficients"]][["game"]]
+  
+  next_game_forecasted <<- data.frame(
+    game = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21),
+    hits = c(batter_stats$hits[1],batter_stats$hits[2],batter_stats$hits[3],batter_stats$hits[4],batter_stats$hits[5],batter_stats$hits[6],batter_stats$hits[7],batter_stats$hits[8],batter_stats$hits[9],batter_stats$hits[10],batter_stats$hits[11],batter_stats$hits[12],batter_stats$hits[13],batter_stats$hits[14],batter_stats$hits[15],batter_stats$hits[16],batter_stats$hits[17],batter_stats$hits[18],batter_stats$hits[19],batter_stats$hits[20],(x_avg+y_avg*5))
+  )
+  
+  plot(my_forecast, type = "l", main = games_ng_title, xlab = "Game", ylab = "Hits")
 }
 
 fetch_mlb_player_stats <- function() {
   batter_stats <<- data.frame()
   
-  #for (i in gamesPlayedIDs) {
-  #batter_stats <<- bind_rows(batter_stats, mlb_player_game_stats(person_id = p, game_pk = i))
-  #}
+  for (i in gamesPlayedML) {
+    batter_stats <<- bind_rows(batter_stats, mlb_player_game_stats(person_id = p, game_pk = i))
+  }  
   
-  batter_stats <<- fg_batter_game_logs(fangraphsId, 2024)
+  batter_stats <<- batter_stats[(batter_stats$group %in% "hitting"),]
+  
+  row.names(batter_stats) <<- 1:nrow(batter_stats)
   
   hitter_forecast_avg <<- data.frame(
-    set = c(1, 2, 3, 4, 5),
-    hits = c(mean(batter_stats$H[21:25]), mean(batter_stats$H[16:20]), mean(batter_stats$H[11:15]), mean(batter_stats$H[6:10]), mean(batter_stats$H[1:5]))
+    game = c(1, 2, 3, 4),
+    bavg = c(sum(batter_stats$hits[16:20])/sum(batter_stats$at_bats[16:20]), sum(batter_stats$hits[11:15])/sum(batter_stats$at_bats[11:15]), sum(batter_stats$hits[6:10])/sum(batter_stats$at_bats[6:10]), sum(batter_stats$hits[1:5])/sum(batter_stats$at_bats[1:5]))
+  )
+  
+  hitter_forecast_next_game <<- data.frame(
+    game = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+    hits = batter_stats$hits[1:20],
+    abs = batter_stats$at_bats[1:20]
+  )
+  
+  nwd_avg <<- data.frame(game = c(5, 6))
+  nwd_ng <<- data.frame(game = c(21))
+  
+  ba <- sum(hitter_forecast_next_game$hits)/sum(batter_stats$at_bats[1:20])
+  firstname <<- (selectedPlayer$name_first)
+  lastname <<- (selectedPlayer$name_last)
+  games_ng_title <<- paste0(firstname," ", lastname, " - Batter Forecast: BA ", round(ba, digits=3))
+}
+
+fetch_triplea_player_stats <- function() {
+  batter_stats <<- data.frame()
+  
+  for (i in gamesPlayedAAA) {
+    batter_stats <<- bind_rows(batter_stats, mlb_player_game_stats(person_id = p, game_pk = i))
+  }  
+  
+  batter_stats <<- batter_stats[(batter_stats$group %in% "hitting"),]
+  
+  row.names(batter_stats) <<- 1:nrow(batter_stats)
+  
+  hitter_forecast_avg <<- data.frame(
+    set = c(1, 2, 3, 4),
+    hits = c(mean(batter_stats$hits[16:20]), mean(batter_stats$hits[11:15]), mean(batter_stats$hits[6:10]), mean(batter_stats$hits[1:5]))
   )
   
   hitter_forecast_10_games <<- data.frame(
-    game = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30),
-    hits = rev(batter_stats$H[1:30])
+    game = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+    hits = batter_stats$hits[1:20]
   )
   
-  nwd_avg <<- data.frame(set = c(6, 7))
-  nwd_10 <<- data.frame(game = c(31, 32, 33, 34, 35, 36, 37, 38, 39, 40))
+  nwd_avg <<- data.frame(set = c(5, 6))
+  nwd_10 <<- data.frame(game = c(21, 22, 23, 24, 25, 26, 27, 28, 29, 30))
   
+  ba <- sum(hitter_forecast_10_games$hits)/sum(batter_stats$at_bats[1:20])
   firstname <<- (selectedPlayer$name_first)
   lastname <<- (selectedPlayer$name_last)
-  games_10_title <<- paste0(firstname," ", lastname, " - Batter Forecast")
+  games_10_title <<- paste0(firstname," ", lastname, " - Batter Forecast: BA ", round(ba, digits=3))
+}
+
+fetch_doublea_player_stats <- function() {
+  batter_stats <<- data.frame()
+  
+  for (i in gamesPlayedAA) {
+  batter_stats <<- bind_rows(batter_stats, mlb_player_game_stats(person_id = p, game_pk = i))
+  }  
+  
+  batter_stats <<- batter_stats[(batter_stats$group %in% "hitting"),]
+  
+  row.names(batter_stats) <<- 1:nrow(batter_stats)
+
+  hitter_forecast_avg <<- data.frame(
+    set = c(1, 2, 3, 4),
+    hits = c(mean(batter_stats$hits[16:20]), mean(batter_stats$hits[11:15]), mean(batter_stats$hits[6:10]), mean(batter_stats$hits[1:5]))
+  )
+  
+  hitter_forecast_10_games <<- data.frame(
+    game = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+    hits = batter_stats$hits[1:20]
+  )
+  
+  nwd_avg <<- data.frame(set = c(5, 6))
+  nwd_10 <<- data.frame(game = c(21, 22, 23, 24, 25, 26, 27, 28, 29, 30))
+  
+  ba <- sum(hitter_forecast_10_games$hits)/sum(batter_stats$at_bats[1:20])
+  firstname <<- (selectedPlayer$name_first)
+  lastname <<- (selectedPlayer$name_last)
+  games_10_title <<- paste0(firstname," ", lastname, " - Batter Forecast: BA ", round(ba, digits=3))
+}
+
+fetch_player_stats <- function(level) {
+  if (level == 1) {
+    getGamesPlayedML()
+    fetch_mlb_player_stats()
+  } else if (level == 2) {
+    getGamesPlayedAAA()
+    fetch_triplea_player_stats()
+  } else if (level == 3) {
+    getGamesPlayedAA()
+    fetch_doublea_player_stats()
+  } else {
+    print("Invalid level entered.")
+  }
 }
 
 selectPlayer <- function(id) {
@@ -65,10 +187,8 @@ selectPlayer <- function(id) {
   fangraphsId <<- selectedPlayer$key_fangraphs
   bbrefId <<- selectedPlayer$key_bbref
   bbrefminorsId <<- selectedPlayer$key_bbref_minors
+  mlbid <<- selectedPlayer$key_mlbam
 }
-
-#Set date
-t <- "2024-07-26"
 
 #Load PBP to find games played in
 load("data/pbp.Rdata")
@@ -80,11 +200,13 @@ getMLBTeams()
 getRoster(134)
 
 #Select Player from currentRoster (use "person_id")
-## Nick Gonzales: 693304
-selectPlayer(693304)
+selectPlayer(663647)
 
 #Get game stats for games played
-fetch_mlb_player_stats()
+fetch_player_stats(1)
 
-#Using past 30 games to forecast hits for next 10 games
-plot_forecast_10_game()
+#Forecast hit total for next game
+plot_forecast_next_game_hits()
+
+#Forecast batting average trend for next 2 series
+plot_forecast_avg()
